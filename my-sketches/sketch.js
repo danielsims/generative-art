dea3935bd5c75fab140630bcf25d365b86e1648a
Daniel Sims <daniel.sims.02@gmail.com>
2022-01-09 15:45:44 +1000

    Cleanup sketch.js

--- my-sketches/sketch.js
+++ my-sketches/sketch.js
@@ -20,7 +20,6 @@ const settings = {
 
 canvasSketch(() => {
   
-
   const drawWaveform = (p5, width, height, yoff) => {
 
     let xoff = 0.0;
@@ -29,14 +28,10 @@ canvasSketch(() => {
       p5.vertex(-10, height+10);
     
       for ( x=0; x<(width+10); x++ ) {
-
         xoff = xoff + p5.random(0.001, 0.015);
-
         let y = yoff + p5.noise(xoff) * 100;
-    
-        //Create Waveform Line from x(0) to edge of screen.
         p5.vertex (x, y);
-        }
+      }
     
       p5.vertex (width+10, height+10);
     p5.endShape();
@@ -48,7 +43,6 @@ canvasSketch(() => {
     p5.stroke(bgColor.r, bgColor.g, bgColor.b)
     p5.ellipse(width/2, height/2, 1000, 1000);
   }
-
 
   // Draw with p5.js
   return ({ p5, time, width, height }) => {
@@ -63,11 +57,8 @@ canvasSketch(() => {
 
     for (i = p5.random(5,20);i < 100;i++) {
 
-
       let rand = Math.floor(p5.random(0 + 1, 4)) + 0;
       fillColor = colorPallete[rand]
-      
-
       p5.fill(fillColor.r, fillColor.g, fillColor.b);
 
       drawWaveform(p5, width, height, (i * p5.random(10,15)))
