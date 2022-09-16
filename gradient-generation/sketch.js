e3e4ca05109198ea203646ea60896c99e7a85883
Daniel Sims <daniel.sims.02@gmail.com>
2022-09-16 23:29:21 +1000

    Gradient generation

---
+++ gradient-generation/sketch.js
@@ -0,0 +1,34 @@
+const canvasSketch = require('canvas-sketch');
+
+// Sketch parameters
+const settings = {
+  dimensions: 'a4',
+  pixelsPerInch: 300,
+  units: 'in'
+};
+
+// Artwork function
+const sketch = () => {
+  return ({ context, width, height }) => {
+    // Margin in inches
+    const margin = 1 / 4;
+
+    // Off-white background
+    context.fillStyle = 'hsl(0, 0%, 98%)';
+    context.fillRect(0, 0, width, height);
+
+    // Gradient foreground
+    const fill = context.createLinearGradient(0, 0, width, height);
+    fill.addColorStop(0, 'hsla(150, 80%, 50%, 80%)');
+    fill.addColorStop(0.4, 'hsla(0, 80%, 50%, 80%)');
+    fill.addColorStop(0.7, 'hsla(0, 80%, 50%, 80%)');
+    fill.addColorStop(1, 'hsla(0, 80%, 0%, 80%)');
+
+    // Fill rectangle
+    context.fillStyle = fill;
+    context.fillRect(margin, margin, width - margin * 2, height - margin * 2);
+  };
+};
+
+// Start the sketch
+canvasSketch(sketch, settings);
